4578915b036a5fc6fabd487abcddebd9eecc843f
Ritter1111 <86166867+Ritter1111@users.noreply.github.com>
2023-04-15 00:48:21 +0300

    feat: solve task matrix sum

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,9 +12,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(s1, s2 ) {
+  let count = 0;
+  s1 = s1.split('');
+  s2 = s2.split('');
  for(let i=0; i<s1.length;i++){
-  
+  for(let j=0; j<s2.length; j++){
+    if(s1[i] == s2[j]){
+      count++
+      s2.splice(s2.indexOf(s1[i]), 1)
+     break;
+    }
+  }
  }
+ return count
 }
 
 module.exports = {
